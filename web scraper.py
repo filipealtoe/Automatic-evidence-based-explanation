bec45b11c71d740b77af2f2dd9f01ad6dcd926ad
filipealtoe <filipe.altoe@gmail.com>
2024-11-19 15:56:01 +0000

    Added option to log dataframe to csv file

--- web scraper.py
+++ web scraper.py
@@ -54,9 +54,32 @@ def scrape_website_text(url: str) -> str:
         logging.error(f"An unexpected error occurred: {e}")
         return ""
 
+def log_scraped_info(df, start, end, args):
+    columns = ['claim', 'label', 'decomposed_question', 'decomposed_justification', 'evidence_url', 'scraped_text']
+    all_data = []
+    data_point = {}
+    for i in tqdm(range(start, end)):
+        data_point['claim'] = df.iloc[i]['claim']
+        data_point['label'] = df.iloc[i]['label']
+        j = 0
+        for search_hit in df.iloc[i]['decomposed_search_hits']:
+            data_point['decomposed_question'] = search_hit['decomposed_question']
+            data_point['decomposed_justification'] = search_hit['decomposed_justification']
+            for page_info in search_hit['pages_info']:
+                data_point['evidence_url'] = page_info['page_url']
+                try:
+                    data_point['scraped_text'] = page_info['scraped_text']
+                except:
+                    pass
+                all_data.append(data_point.copy())
+                data_point = {}
+            j = j + 1
+    df1 = pd.DataFrame(all_data)
+    csv_file = args.input_path.split(".json")[0] + ".csv"
+    df1.to_csv(csv_file,sep='\t', encoding='utf-8', index=False, header=True)
+
 def main(args):
     df = pd.read_json(args.input_path, lines=True)
-
     start = 0 if not args.start else args.start
     end = len(df) if not args.end else args.end
 
@@ -71,12 +94,14 @@ def main(args):
                 page_info['scraped_text'] = scraped_text
                 pages_info[page_info_index] = page_info
                 page_info_index = page_info_index + 1
-            search_hits[search_hit_index] = pages_info
+            search_hits[search_hit_index]['pages_info'] = pages_info
             search_hit_index = search_hit_index + 1
         df.at[i, 'decomposed_search_hits'] = search_hits
 
 
     df.to_json(args.output_path, orient='records', lines=True)
+    if args.log_scraped_info:
+        log_scraped_info(df, start, end, args)
 
 
 if __name__ == '__main__':
@@ -86,5 +111,6 @@ if __name__ == '__main__':
     parser.add_argument('--start', type=int, default=None)
     parser.add_argument('--end', type=int, default=None)
     parser.add_argument('--scrape_top_hits', type=int, default=10)
+    parser.add_argument('--log_scraped_info', type=int, default=0)
     args = parser.parse_args()
     main(args)
